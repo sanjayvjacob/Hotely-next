433b91a742e716329f919bcf0093fd28d05ce085
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-06-02 11:18:25 +0100

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={openSans.className}>{children}</body>
     </html>
   );
