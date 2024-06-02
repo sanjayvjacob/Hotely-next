96ded7c60a5ffde8f4599c528b32c2efe539579c
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-06-02 16:15:11 +0100

    image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,8 @@
 import SearchBox from "./SearchBox"
-import BannerImage from "/public/hero/bg-hero2.png"
 
 const Hero = () => {
   return (
-    <section 
-    className="pt-24 pb-12 xl:py-0 xl:h-[1087px] bg-hero2 xl:bg-hero bg-cover bg-center bg-no-repeat flex justify-center items-center"
-    style={{backgroundImage: `url(${BannerImage})`}}>
+    <section className="pt-24 pb-12 xl:py-0 xl:h-[1087px] bg-hero2 xl:bg-hero bg-cover bg-center bg-no-repeat flex justify-center items-center">
       <div className="container mx-auto flex items-center justify-center xl:justify-start">
         <SearchBox/>
       </div>
